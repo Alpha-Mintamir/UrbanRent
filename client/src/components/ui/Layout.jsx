ad9bf2719acc82f5a508908765d8defd1f8507ac
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2025-03-03 12:11:30 +0300

    Home page added

--- client/src/components/ui/Layout.jsx
+++ client/src/components/ui/Layout.jsx
@@ -3,12 +3,14 @@ import { Outlet } from 'react-router-dom';
 
 import { Header } from './Header';
 import Footer from './Footer';
+import Home from '@/pages/Home';
 
 const Layout = () => {
   return (
     <>
       <Header />
       <div className="mx-auto flex min-h-screen max-w-screen-xl flex-col">
+        <Home />
         <Outlet />
       </div>
       <Footer />
